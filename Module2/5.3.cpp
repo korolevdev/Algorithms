26ff8e85bd1a02aedc91bb8aa81a159074441806
korolevdev <korolevdev@gmail.com>
2016-04-22 19:28:56 +0300

    fixed 5.3

--- Module2/5.3.cpp
+++ Module2/5.3.cpp
@@ -3,47 +3,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int64_t splitAndMerge(int *arr, int start, int end, int *buffer) {
-    if (end - start == 0)
+template<class T>
+bool compare(const T &obj1, const T &obj2) {
+    return obj1 <= obj2;
+}
+
+template<class T>
+int64_t merge(T *arr, int first, int last, T *buf, bool (*compare) (const T &, const T &)) {
+    if (last - first == 0)
         return 0;
 
-    int half1_end = (end - start) / 2 + start;
-    int half2_start = half1_end + 1;
-    int64_t inv1 = splitAndMerge(arr, start, half1_end, buffer);
-    int64_t inv2 = splitAndMerge(arr, half2_start, end, buffer);
+    int half1_last = (last - first) / 2 + first;
+    int half2_first = half1_last + 1;
+    int64_t inv1 = merge(arr, first, half1_last, buf, compare);
+    int64_t inv2 = merge(arr, half2_first, last, buf, compare);
     int64_t inv3 = 0;
 
-    int j = start, k = half2_start;
+    int j = first, k = half2_first;
 
-    for (int i = start; i <= end; ++i)
-        if (j <= half1_end && k <= end) {
+    for (int i = first; i <= last; ++i)
+        if (j <= half1_last && k <= last) {
             if (arr[j] <= arr[k]) {
-                buffer[i] = arr[j];
+                buf[i] = arr[j];
                 ++j;
             } else {
-                inv3 += (half1_end - j + 1);
-                buffer[i] = arr[k];
+                inv3 += (half1_last - j + 1);
+                buf[i] = arr[k];
                 ++k;
             }
         } else {
-            if (j <= half1_end) {
-                buffer[i] = arr[j];
+            if (j <= half1_last) {
+                buf[i] = arr[j];
                 ++j;
             } else {
-                buffer[i] = arr[k];
+                buf[i] = arr[k];
                 ++k;
             }
         }
 
-    for (int i = start; i <= end; ++i)
-        arr[i] = buffer[i];
+    for (int i = first; i <= last; ++i)
+        arr[i] = buf[i];
 
     return inv1 + inv2 + inv3;
 }
 
-int64_t mergeSort(int *arr, int size) {
-    int *buf = (int*) malloc((size + 1) * sizeof(int));;
-    int64_t inv = splitAndMerge(arr, 0, size - 1, buf);
+template<class T>
+int64_t mergeSort(T *arr, int size, bool (*compare) (const T &, const T &)) {
+    T *buf = (T*) malloc((size + 1) * sizeof(T));;
+    int64_t inv = merge(arr, 0, size - 1, buf, compare);
+
+    free(buf);
     return inv;
 }
 
@@ -59,7 +68,10 @@ int main() {
         }
     }
 
-    int64_t inv = mergeSort(arr, i);
+    int64_t inv = mergeSort(arr, i, compare);
+
     printf("%ld", inv);
+
+    free(arr);
     return 0;
 }
